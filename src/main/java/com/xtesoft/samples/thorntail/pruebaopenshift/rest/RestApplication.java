2d8645b84b66c6dd7cb2fd0027f9eb1581408c16
Tadeo Rivera <xtecuan@gmail.com>
2021-07-09 14:37:47 -0600

    Commit inicial

---
+++ src/main/java/com/xtesoft/samples/thorntail/pruebaopenshift/rest/RestApplication.java
@@ -0,0 +1,8 @@
+package com.xtesoft.samples.thorntail.pruebaopenshift.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class RestApplication extends Application {
+}
